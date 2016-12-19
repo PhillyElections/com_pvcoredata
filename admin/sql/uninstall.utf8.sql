af8c5b6c5c3cea327c3815f62e5dd196862f7260
matt murphy <mattyhead@gmail.com>
2016-12-19 15:51:43 -0500

    fixing drops

--- admin/sql/uninstall.utf8.sql
+++ admin/sql/uninstall.utf8.sql
@@ -1,24 +1,19 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-DROP TABLE IF EXISTS `#__pv_address_to_office`;
-DROP TABLE IF EXISTS `#__pv_address_to_person`;
+DROP TABLE IF EXISTS `#__pv_address_xrefs`;
 DROP TABLE IF EXISTS `#__pv_address_to_seat`;
 DROP TABLE IF EXISTS `#__pv_addresses`;
-DROP TABLE IF EXISTS `#__pv_candidates`;
 DROP TABLE IF EXISTS `#__pv_cycle_year`;
 DROP TABLE IF EXISTS `#__pv_cycles`;
 DROP TABLE IF EXISTS `#__pv_elections`;
-DROP TABLE IF EXISTS `#__pv_link_to_office`;
-DROP TABLE IF EXISTS `#__pv_link_to_person`;
-DROP TABLE IF EXISTS `#__pv_link_to_seat`;
+DROP TABLE IF EXISTS `#__pv_link_xrefs`;
 DROP TABLE IF EXISTS `#__pv_link_types`;
 DROP TABLE IF EXISTS `#__pv_links`;
 DROP TABLE IF EXISTS `#__pv_officers`;
 DROP TABLE IF EXISTS `#__pv_offices`;
 DROP TABLE IF EXISTS `#__pv_parties`;
 DROP TABLE IF EXISTS `#__pv_persons`;
-DROP TABLE IF EXISTS `#__pv_reports`;
-DROP TABLE IF EXISTS `#__pv_terms`;
-DROP TABLE IF EXISTS `#__pv_votes`;
+DROP TABLE IF EXISTS `#__pv_seats`;
+DROP TABLE IF EXISTS `#__pv_terms
 
 SET FOREIGN_KEY_CHECKS=1;

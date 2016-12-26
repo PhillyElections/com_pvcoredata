21d2271bbb90d7885197e258965db3591ae4caf3
matt murphy <mattyhead@gmail.com>
2016-12-26 07:12:37 -0500

    woops.  we moved that crap into nominations

--- admin/sql/install.utf8.sql
+++ admin/sql/install.utf8.sql
@@ -364,9 +364,8 @@ UPDATE `#__pv_offices` set `ordering` = `id`;
   State Representative
   State Senator
   State Treasurer
-
-UPDATE `#__pv_offices` set `fees` = , `signatures` = ;
 */
+
 /* ===================== pv_terms ===================== */
 INSERT INTO `#__pv_terms` 
   (`offset`, `length`, `name`, `published`, `created` )
